9ab8e3c3874e8baec134cd0a0e0f37eae7687032
unknown <Admin@CSLAB128.CSSERVER.local>
2018-02-15 16:09:35 +0800

    finished problem 2

--- ISpyChallenge2/src/ispychallenge2/ISpyChallenge2.java
+++ ISpyChallenge2/src/ispychallenge2/ISpyChallenge2.java
@@ -1,21 +1,71 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ispychallenge2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  *
  * @author Admin
  */
-public class ISpyChallenge2 {
+public class ISpyChallenge2 
+{
+    static BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+    
+    static boolean isPalindrome(String string1, String string2)
+    {
+        return string1.equals(string2);
+    }
+    
+    static String reverseInputString(String input)
+    {
+        String output = "";
+        char[] inputChar = input.toCharArray();
+        
+        for(int i = inputChar.length-1; i>=0; i--)
+        {
+            output+=inputChar[i];
+        }
+        
+        return output;
+    }
+    
+    static String cleanString(String input)
+    {
+        String output = "";
+        char[] inputChar = input.toLowerCase().toCharArray();
+        
+        for(char c:inputChar)
+        {
+            if(Character.isAlphabetic(c))
+            {
+                output+=c;
+            }
+        }
+        
+        return output;
+    }
+    
+    public static void main(String[] args) throws IOException
+    {
+        String input;
+        String cleanInput, reversedInput;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.print("Enter a entence to see if it is a palindrome or not: ");
+        input = console.readLine();
+        
+        cleanInput = cleanString(input);
+        reversedInput = reverseInputString(cleanInput);
+        
+        if(isPalindrome(cleanInput, reversedInput))
+        {
+            System.out.println("The sentence '"+input+"' is a palindrome");
+        }
+        else
+        {
+            System.out.println("The sentence '"+input+"' is not a palindrome");
+        }
     }
     
 }
